592241b170105c3bfe4f72605ef8bed855f274bd
Joao Victor <jov9051@outlook.com>
2022-01-23 15:57:45 -0300

    form-container div created inside app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <div className="form-container"></div>
+        </div>
+    );
 }
 
 export default App;
