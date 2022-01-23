321163ac3cd4976ee5804b661ac908711b50b281
Joao Victor <jov9051@outlook.com>
2022-01-23 16:03:26 -0300

    funçao handleCity criada adicionando valor de input a City

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import { useState } from 'react';
 
 function App() {
     let [City, setCity] = useState('');
+
+    function handleCity(event) {
+        setCity(event.target.value);
+    }
     return (
         <div className="App">
             <h1>Weather App</h1>
             <div className="form-container">
-                <input type="text" placeholder="Localização" name="inputCity" />
+                <input
+                    onChange={handleCity}
+                    type="text"
+                    placeholder="Localização"
+                    name="inputCity"
+                />
                 <button type="submit">Pesquisar</button>
             </div>
         </div>
