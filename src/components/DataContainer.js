9c99b8cf6a65d01973be3d7f919b3faa8bf25dc9
Joao Victor <jov9051@outlook.com>
2022-01-24 18:33:24 -0300

    estilo aplicado a div data-container

--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -10,23 +10,23 @@ function DataContainer(props) {
                 src={MainLogo}
                 alt="weather-icon"
             ></img>
-            <p>
+            <h2>
                 {props.ApiData.name} - {props.ApiData.sys.country}
-            </p>
-            <p>Tempo: {props.ApiData.weather[0].main}</p>
-            <p>Descrição: {props.ApiData.weather[0].description}</p>
+            </h2>
+            <h3>{props.ApiData.weather[0].main}</h3>
+            <h4>{props.ApiData.weather[0].description}</h4>
             <p>Temperatura: {(props.ApiData.main.temp - 273.15).toFixed()}°C</p>
             <p>
                 Temperatura maxima:{' '}
-                {(props.ApiData.main.temp_max - 273.15).toFixed()} °C
+                {(props.ApiData.main.temp_max - 273).toFixed()} °C
             </p>
             <p>
                 Temperatura minima:{' '}
-                {(props.ApiData.main.temp_min - 273.15).toFixed()} °C
+                {(props.ApiData.main.temp_min - 273).toFixed()} °C
             </p>
             <p>
                 Sensação termica:{' '}
-                {(props.ApiData.main.feels_like - 273.15).toFixed()} °C
+                {(props.ApiData.main.feels_like - 273).toFixed()} °C
             </p>
             <p>Humidade: {props.ApiData.main.humidity}%</p>
         </div>
