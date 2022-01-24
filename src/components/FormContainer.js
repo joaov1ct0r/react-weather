3a9f166795fe8684ea51501d189eaee4e7df566c
Joao Victor <jov9051@outlook.com>
2022-01-24 12:47:07 -0300

    codigos da div FormContainer movido para o component FormContainer

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,3 +1,52 @@
-function FormContainer(props) {}
+import { FiSearch } from 'react-icons/fi';
+
+import { useState } from 'react';
+
+import DataContainer from './components/DataContainer';
+
+function FormContainer(props) {
+    let [City, setCity] = useState('');
+
+    let [ApiData, setApiData] = useState({});
+
+    function handleCity(event) {
+        setCity(event.target.value);
+    }
+
+    function handleSearch() {
+        try {
+            const API_KEY = '11793562357b04c38a206416e8384ea1';
+
+            const URL = `http://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${API_KEY}`;
+
+            fetch(URL)
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    setApiData(data);
+                });
+        } catch {
+            alert('Houve algum erro!');
+        }
+    }
+    return (
+        <div className="form-container">
+            <input
+                onChange={handleCity}
+                type="text"
+                placeholder="Localização"
+                name="inputCity"
+            />
+            <button onClick={handleSearch} type="submit">
+                <FiSearch size={20} color="#FFF"></FiSearch>
+            </button>
+            {Object.keys(ApiData).length > 0 && (
+                <DataContainer ApiData={ApiData}></DataContainer>
+            )}
+        </div>
+    );
+}
 
 export default FormContainer;
