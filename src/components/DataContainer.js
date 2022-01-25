5be4683c37000a184bc1f088d59d7a0187964f43
Joao Victor <jov9051@outlook.com>
2022-01-25 14:37:36 -0300

    img icone adicionada a DataContainer

--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -1,9 +1,14 @@
 function DataContainer(props) {
+    console.log(props.ApiData);
     return (
         <div className="data-container">
             <h2>
                 {props.ApiData.name} - {props.ApiData.sys.country}
             </h2>
+            <img
+                src={require(`../assets/icons/${props.ApiData.weather[0].icon}.png`)}
+                alt="weather-icon"
+            ></img>
             <h3>
                 Temperatura: {(props.ApiData.main.temp - 273.15).toFixed()}°C
             </h3>
