0e9f9dd225ed1021c96c43f301eafe41b0612886
Joao Victor <jov9051@outlook.com>
2022-11-20 15:41:00 -0300

    build: prettier instalado

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import { useState } from 'react';
-import './App.css';
-import IApiData from './interfaces/IApiData';
-import FormContainer from './components/FormContainer.';
-import DataContainer from './components/DataContainer';
+import React, { useState } from "react";
+import "./App.css";
+import IApiData from "./interfaces/IApiData";
+import FormContainer from "./components/FormContainer.";
+import DataContainer from "./components/DataContainer";
 
 function App() {
-    let [City, setCity] = useState<string>('');
+  const [City, setCity] = useState<string>("");
 
-    let [ApiData, setApiData] = useState<IApiData | null>(null);
+  const [ApiData, setApiData] = useState<IApiData | null>(null);
 
-    function handleCity(event: React.ChangeEvent<HTMLInputElement>): void {
-        setCity(event.target.value);
+  function handleCity(event: React.ChangeEvent<HTMLInputElement>): void {
+    setCity(event.target.value);
+  }
+
+  async function handleSearch(): Promise<void> {
+    try {
+      const URL = `${process.env.REACT_APP_API_URL}?q=${City}&appid=${process.env.REACT_APP_API_KEY}`;
+
+      const request: Response = await fetch(URL);
+
+      const response: IApiData = await request.json();
+
+      setApiData(response);
+    } catch (error: unknown) {
+      console.log(error);
     }
-
-    async function handleSearch(): Promise<void> {
-        try {
-            const URL: string = `${process.env.REACT_APP_API_URL}?q=${City}&appid=${process.env.REACT_APP_API_KEY}`;
-
-            const request: Response = await fetch(URL);
-
-            const response: IApiData = await request.json();
-
-            setApiData(response);
-        } catch (error: any){
-           console.log(error)
-        }
-    }
-    return (
-        <div className="App">
-          <h1>Weather App</h1>
-          <FormContainer handleCity={handleCity} handleSearch={handleSearch} />
-          { ApiData === null ? <></> :
-          <DataContainer ApiData={ApiData} />
-          }
-        </div>
-    );
+  }
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <FormContainer handleCity={handleCity} handleSearch={handleSearch} />
+      {ApiData === null ? <></> : <DataContainer ApiData={ApiData} />}
+    </div>
+  );
 }
 
 export default App;
--- src/components/FormContainer..tsx
+++ src/components/FormContainer..tsx
@@ -1,20 +1,24 @@
-import { FiSearch } from 'react-icons/fi';
-import IFormContainerProps from '../interfaces/IFormContainerProps';
+import React from "react";
+import { FiSearch } from "react-icons/fi";
+import IFormContainerProps from "../interfaces/IFormContainerProps";
 
-function FormContainer ({ handleCity, handleSearch}: IFormContainerProps): JSX.Element {
+function FormContainer({
+  handleCity,
+  handleSearch,
+}: IFormContainerProps): JSX.Element {
   return (
     <div className="form-container">
-              <input
-                onChange={handleCity}
-                type="text"
-                placeholder="Localização"
-                name="inputCity"
-              />
-              <button onClick={handleSearch} type="submit">
-                <FiSearch size={20} color="#FFF"></FiSearch>
-              </button>
-          </div>
-  )
+      <input
+        onChange={handleCity}
+        type="text"
+        placeholder="Localização"
+        name="inputCity"
+      />
+      <button onClick={handleSearch} type="submit">
+        <FiSearch size={20} color="#FFF"></FiSearch>
+      </button>
+    </div>
+  );
 }
 
 export default FormContainer;
