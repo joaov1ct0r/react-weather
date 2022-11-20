9fc3bb55b006c542e0d93dfa1d4801563c0f46a2
Joao Victor <jov9051@outlook.com>
2022-11-20 15:06:28 -0300

    feat: component DataContainer adicionado a App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import IApiData from './interfaces/IApiData';
 import FormContainer from './components/FormContainer.';
+import DataContainer from './components/DataContainer';
 
 function App() {
     let [City, setCity] = useState<string>('');
@@ -30,24 +31,7 @@ function App() {
           <h1>Weather App</h1>
           <FormContainer handleCity={handleCity} handleSearch={handleSearch} />
           { ApiData === null ? <></> :
-          <div className="data-container">
-            <h2>
-                {ApiData.name} - {ApiData.sys.country}
-            </h2>
-            <img
-                src={require(`./assets/icons/${ApiData.weather[0].icon}.png`)}
-                alt="weather-icon"
-            ></img>
-            <h3>
-                Temperatura: {(ApiData.main.temp - 273.15).toFixed()}°C
-            </h3>
-            <h3>
-                {(ApiData.main.temp_min - 273.15).toFixed()} |{' '}
-                {(ApiData.main.temp_max - 273.15).toFixed()}
-            </h3>
-            <h3>{ApiData.weather[0].main}</h3>
-            <h4>{ApiData.weather[0].description}</h4>
-          </div>
+          <DataContainer ApiData={ApiData} />
           }
         </div>
     );
