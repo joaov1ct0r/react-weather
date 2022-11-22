2fd0cf117c7e6adf320d253869f3f16185ec7e34
Joao Victor <jov9051@outlook.com>
2022-11-22 20:19:13 -0300

    feat: hook useContext adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
-import React, { useState } from "react";
-import "./styles/App.css";
-import IApiData from "./interfaces/IApiData";
+import React, { useContext } from "react";
 import FormContainer from "./components/FormContainer.";
 import DataContainer from "./components/DataContainer";
+import { FormProvider, FormContext } from "./components/FormContext";
+import IFormContextType from "./interfaces/IFormContextType";
+import "./styles/App.css";
 
 function App() {
-  const [City, setCity] = useState<string>("");
-
-  const [ApiData, setApiData] = useState<IApiData | null>(null);
-
-  function handleCity(event: React.ChangeEvent<HTMLInputElement>): void {
-    setCity(event.target.value);
-  }
-
-  async function handleSearch(): Promise<void> {
-    try {
-      const URL = `${process.env.REACT_APP_API_URL}?q=${City}&appid=${process.env.REACT_APP_API_KEY}`;
-
-      const request: Response = await fetch(URL);
-
-      const response: IApiData = await request.json();
-
-      setApiData(response);
-    } catch (error: unknown) {
-      console.log(error);
-    }
-  }
+  const { ApiData } = useContext(FormContext) as IFormContextType;
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <FormContainer handleCity={handleCity} handleSearch={handleSearch} />
-      {ApiData === null ? <></> : <DataContainer ApiData={ApiData} />}
+      <FormProvider>
+        <FormContainer />
+        {ApiData === null ? <></> : <DataContainer />}
+      </FormProvider>
     </div>
   );
 }
