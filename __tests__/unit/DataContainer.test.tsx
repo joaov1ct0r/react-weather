901d88486c0c09da9c95453df6f264e76835de34
Joao Victor <jov9051@outlook.com>
2022-11-26 18:51:45 -0300

    test: teste para component DataContainer renderizar ApiData, criado

---
+++ __tests__/unit/DataContainer.test.tsx
@@ -0,0 +1,75 @@
+import React, { useContext } from "react";
+import "@testing-library/jest-dom";
+import { render, renderHook, screen } from "@testing-library/react";
+import DataContainer from "../../src/components/DataContainer";
+import IFormContextProps from "../../src/interfaces/IFormContextProps";
+import { FormContext, FormProvider } from "../../src/components/FormContext";
+import IFormContextType from "../../src/interfaces/IFormContextType";
+import { act } from "react-dom/test-utils";
+
+describe("data container", () => {
+  it("should render ApiData", () => {
+    render(<DataContainer />);
+
+    const wrapper = ({ children }: IFormContextProps) => (
+      <FormProvider>{children}</FormProvider>
+    );
+
+    const { result } = renderHook(
+      () => useContext(FormContext) as IFormContextType,
+      {
+        wrapper,
+      }
+    );
+
+    act(() => {
+      result.current.setApiData({
+        coord: {
+          lon: 2.3488,
+          lat: 48.8534,
+        },
+        weather: [
+          {
+            id: 800,
+            main: "Clear",
+            description: "clear sky",
+            icon: "01n",
+          },
+        ],
+        base: "stations",
+        main: {
+          temp: 279.94,
+          feels_like: 277.75,
+          temp_min: 277.92,
+          temp_max: 280.58,
+          pressure: 1026,
+          humidity: 91,
+        },
+        visibility: 10000,
+        wind: {
+          speed: 3.09,
+          deg: 260,
+        },
+        clouds: {
+          all: 0,
+        },
+        dt: 1669408145,
+        sys: {
+          type: 1,
+          id: 6550,
+          country: "FR",
+          sunrise: 1669360468,
+          sunset: 1669392060,
+        },
+        timezone: 3600,
+        id: 2988507,
+        name: "Paris",
+        cod: 200,
+      });
+    });
+
+    const dataContainer = screen.getByTestId("data-div");
+
+    expect(dataContainer).toBeInTheDocument();
+  });
+});
--- src/components/DataContainer.tsx
+++ src/components/DataContainer.tsx
@@ -5,26 +5,34 @@ import { FormContext } from "./FormContext";
 function DataContainer(): JSX.Element {
   const { ApiData } = useContext(FormContext) as IFormContextType;
 
-  if (ApiData === null) return <></>;
-
-  return (
-    <div className="data-container">
-      <h2>
-        {ApiData.name} - {ApiData.sys.country}
-      </h2>
-      <img
-        src={require(`../assets/icons/${ApiData.weather[0].icon}.png`)}
-        alt="weather-icon"
-      ></img>
-      <h3>Temperatura: {(ApiData.main.temp - 273.15).toFixed()}°C</h3>
-      <h3>
-        {(ApiData.main.temp_min - 273.15).toFixed()} |{" "}
-        {(ApiData.main.temp_max - 273.15).toFixed()}
-      </h3>
-      <h3>{ApiData.weather[0].main}</h3>
-      <h4>{ApiData.weather[0].description}</h4>
-    </div>
-  );
+  if (ApiData === null) {
+    return (
+      <label className="fragment-div">
+        <div></div>
+      </label>
+    );
+  } else {
+    return (
+      <label className="data">
+        <div className="data-container" data-testid="data-div">
+          <h2>
+            {ApiData?.name} - {ApiData?.sys.country}
+          </h2>
+          <img
+            src={`../components/src/public/assets/icons/${ApiData?.weather[0].icon}.png`}
+            alt="weather-icon"
+          ></img>
+          <h3>Temperatura: {(ApiData?.main.temp - 273.15).toFixed()}°C</h3>
+          <h3>
+            {(ApiData?.main.temp_min - 273.15).toFixed()} |{" "}
+            {(ApiData?.main.temp_max - 273.15).toFixed()}
+          </h3>
+          <h3>{ApiData?.weather[0].main}</h3>
+          <h4>{ApiData?.weather[0].description}</h4>
+        </div>
+      </label>
+    );
+  }
 }
 
 export default DataContainer;
