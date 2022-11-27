2b2cf52b2111cc993a7cdde98bfa8ab61999d9ae
Joao Victor <jov9051@outlook.com>
2022-11-27 17:59:00 -0300

    test: teste para component App renderizar fragment-div, criado

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import FormContainer from "./components/FormContainer";
 import DataContainer from "./components/DataContainer";
 import { FormProvider } from "./components/FormContext";
-// import "./styles/App.css";
 
 function App() {
   return (
