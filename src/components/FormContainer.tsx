f5c4c618d4128738b64d7a42354a5f29d3a16bd5
joaov1ct0r <jov9051@outlook.com>
2023-05-10 22:41:05 -0300

    refactor: FormContext

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -1,26 +1,26 @@
-import React, { useContext } from "react";
-import { FiSearch } from "react-icons/fi";
-import IFormContextType from "../interfaces/IFormContextType";
-import { FormContext } from "./FormContext";
+import React, { useContext } from 'react'
+import { FiSearch } from 'react-icons/fi'
+import IFormContextType from '../interfaces/IFormContextType'
+import { FormContext } from './FormContext'
 
-function FormContainer(): JSX.Element {
+function FormContainer (): JSX.Element {
   const { handleSearch, handleCity } = useContext(
     FormContext
-  ) as IFormContextType;
+  ) as IFormContextType
   return (
-    <div className="form-container" data-testid="form-div">
+    <div className='form-container' data-testid='form-div'>
       <input
         onChange={handleCity}
-        type="text"
-        placeholder="Localização"
-        name="inputCity"
-        data-testid="city-input"
+        type='text'
+        placeholder='Localização'
+        name='inputCity'
+        data-testid='city-input'
       />
-      <button onClick={handleSearch} type="submit" data-testid="search-button">
-        <FiSearch size={20} color="#FFF"></FiSearch>
+      <button onClick={handleSearch} type='submit' data-testid='search-button'>
+        <FiSearch size={20} color='#FFF' />
       </button>
     </div>
-  );
+  )
 }
 
-export default FormContainer;
+export default FormContainer
--- src/components/FormContext.tsx
+++ src/components/FormContext.tsx
@@ -1,30 +1,35 @@
-import React, { useState, createContext } from "react";
-import IApiData from "../interfaces/IApiData";
-import IFormContextType from "../interfaces/IFormContextType";
-import IFormContextProps from "../interfaces/IFormContextProps";
+import React, { useState, createContext } from 'react'
+import IApiData from '../interfaces/IApiData'
+import IFormContextType from '../interfaces/IFormContextType'
+import IFormContextProps from '../interfaces/IFormContextProps'
 
-export const FormContext = createContext<IFormContextType | unknown>({});
+export const FormContext = createContext<IFormContextType | unknown>({})
 
-export function FormProvider(props: IFormContextProps) {
-  const [ApiData, setApiData] = useState<IApiData | null>(null);
+export function FormProvider (props: IFormContextProps): JSX.Element {
+  const [ApiData, setApiData] = useState<IApiData | null>(null)
 
-  const [City, setCity] = useState<string>("");
+  const [City, setCity] = useState<string>('')
 
-  function handleCity(event: React.ChangeEvent<HTMLInputElement>): void {
-    setCity(event.target.value);
+  function handleCity (event: React.ChangeEvent<HTMLInputElement>): void {
+    setCity(event.target.value)
   }
 
-  async function handleSearch(): Promise<void> {
+  async function handleSearch (): Promise<void> {
     try {
-      const URL = `${process.env.REACT_APP_API_URL}?q=${City}&appid=${process.env.REACT_APP_API_KEY}`;
+      if (process.env.REACT_APP_API_URL === undefined || process.env.REACT_APP_API_KEY === undefined) {
+        process.env.REACT_APP_API_URL = 'localhost'
+        process.env.REACT_APP_API_KEY = 'localhost'
+      }
 
-      const request: Response = await fetch(URL);
+      const URL = `${process.env.REACT_APP_API_URL}?q=${City}&appid=${process.env.REACT_APP_API_KEY}`
 
-      const response: IApiData = await request.json();
+      const request: Response = await fetch(URL)
 
-      setApiData(response);
+      const response: IApiData = await request.json()
+
+      setApiData(response)
     } catch (error: unknown) {
-      console.log(error);
+      console.log(error)
     }
   }
 
@@ -34,5 +39,5 @@ export function FormProvider(props: IFormContextProps) {
     >
       {props.children}
     </FormContext.Provider>
-  );
+  )
 }
