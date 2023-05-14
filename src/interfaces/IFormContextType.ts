1f9f463fccf7a9a9d049a9392024ac7230673320
joaov1ct0r <jov9051@outlook.com>
2023-05-14 16:26:40 -0300

    refactor: IFormContextType

--- src/interfaces/IFormContextType.ts
+++ src/interfaces/IFormContextType.ts
@@ -1,10 +1,10 @@
-import IApiData from "./IApiData";
+import IApiData from './IApiData'
 
 export default interface IFormContextType {
-  ApiData: IApiData | null;
-  setApiData: React.Dispatch<React.SetStateAction<IApiData | null>>;
-  City: string;
-  setCity: React.Dispatch<React.SetStateAction<string>>;
-  handleCity(event: React.ChangeEvent<HTMLInputElement>): void;
-  handleSearch(): Promise<void>;
+  ApiData: IApiData | null
+  setApiData: React.Dispatch<React.SetStateAction<IApiData | null>>
+  City: string
+  setCity: React.Dispatch<React.SetStateAction<string>>
+  handleCity: (event: React.ChangeEvent<HTMLInputElement>) => void
+  handleSearch: () => Promise<void>
 }
